c7e1cc0a0ca9b24eedb0528556c8680fca59b280
Sebastian Gudiño <sebasatiangudi234@gmail.com>
2025-06-17 00:39:41 -0500

    Initial commit

---
+++ src/main/java/com/dev/kuntur/KunturApplication.java
@@ -0,0 +1,13 @@
+package com.dev.kuntur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunturApplication.class, args);
+	}
+
+}
